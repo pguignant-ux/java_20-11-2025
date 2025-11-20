7cbfd76e5cf239284200e5a2e3c8b037672bd61e
Patrice Guignant <pguignant@gmail.com>
2025-11-20 11:46:19 +0100

    java_exo_11h45

--- lethal.js
+++ lethal.js
@@ -1,16 +1,22 @@
 const input1 = document.getElementById("to_do")
 const btw = document.getElementById("clic")
 const texte = document.getElementById("result")
-
+const tache_enc = document.getElementById("tache_ec")
+const progress = document.getElementById("champ1")
 function readText() {
     const text = document.getElementById("to_do").value;
-    document.getElementById('result').textContent = text
+    document.getElementById("champ1").textContent = text
+    result.style.color = "black"
     if (text != "") {
         const tache = document.createElement("button")
         tache.textContent = "ajouter"
-        document.getElementById("in_1").appendChild(tache)
-    } else if ("result" === "")
-        text = "vous n'avez inscrit aucune tache a faire"
+        document.getElementById("en_cours").appendChild(tache)
+    } else {
+        result.textContent = "vous n'avez inscrit aucune tache a faire";
+        result.style.color = "red"
+
+    }
+
 
 
 
